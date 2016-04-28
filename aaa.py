4f575d6a1e0787359cf43518680081c7791565fb
roach <59419914@qq.com>
2016-04-28 23:11:25 +0800

    add aaa.py function main

--- aaa.py
+++ aaa.py
@@ -5,25 +5,32 @@ import time
 import os
 
 
-if os.path.exists('/opt/python/projects/text.txt'):
-	print("the dir is exists")
-	fileinfo = os.stat('text.txt')
-	filemtime = fileinfo.st_mtime
+def firstrun():
+	if os.path.exists('/opt/python/projects/text.txt'):
+		print("the dir is exists")
+		fileinfo = os.stat('text.txt')
+		filemtime = fileinfo.st_mtime
 
-	if (time.time() - filemtime) >= 300:
-		print("too old!")
-		os.remove("/opt/python/projects/text.txt")
-		print("create a new text.txt")
-else:
-	print("dont have this dir")
+		if (time.time() - filemtime) >= 300:
+			print("too old!")
+			os.remove("/opt/python/projects/text.txt")
+			print("create a new text.txt")
+	else:
+		print("dont have this dir")
 
-while True:
-	global gotime
-	gotime = int(str(time.time())[:10])
-	if gotime % 5 == 0:
-	#if int(time.time()[:10]) % 300  == 0:
-		break
-	time.sleep(1)
+	while True:
+		global gotime
+		gotime = int(str(time.time())[:10])
+		if gotime % 5 == 0:
+		#if int(time.time()[:10]) % 300  == 0:
+			break
+		time.sleep(1)
 
-print(gotime)
-print("OK")
+	print(gotime)
+	print("OK")
+
+def main():
+	firstrun()
+
+if __name__ == '__main__':
+	main()

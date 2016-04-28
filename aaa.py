eaf5719427932b743dc14918c3d80b4381c00ecc
roach <59419914@qq.com>
2016-04-28 23:07:02 +0800

    edit aaa.py

--- aaa.py
+++ aaa.py
@@ -11,6 +11,19 @@ if os.path.exists('/opt/python/projects/text.txt'):
 	filemtime = fileinfo.st_mtime
 
 	if (time.time() - filemtime) >= 300:
-		print("too big ,delete it")
+		print("too old!")
+		os.remove("/opt/python/projects/text.txt")
+		print("create a new text.txt")
 else:
 	print("dont have this dir")
+
+while True:
+	global gotime
+	gotime = int(str(time.time())[:10])
+	if gotime % 5 == 0:
+	#if int(time.time()[:10]) % 300  == 0:
+		break
+	time.sleep(1)
+
+print(gotime)
+print("OK")
